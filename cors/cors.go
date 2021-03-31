79f43498191d8c58db4f62bb8df9b4fa95a81283
oneilj57 <james.oneill57@mail.dcu.ie>
2021-03-31 20:48:43 +0100

    Initiated CORS setup

---
+++ cors/cors.go
@@ -0,0 +1,10 @@
+package cors
+
+import "net/http"
+
+/*SetupCORS function will set the http headers required to allow communication with the front end*/
+func SetupCORS(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
