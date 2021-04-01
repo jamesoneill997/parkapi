c99e127fa60d534a70581323722ba214dd125970
oneilj57 <james.oneill57@mail.dcu.ie>
2021-04-01 14:20:37 +0100

    added allow credentials for CORS

--- cors/cors.go
+++ cors/cors.go
@@ -7,4 +7,5 @@ func SetupCORS(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, ParkAIToken")
+	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 }
