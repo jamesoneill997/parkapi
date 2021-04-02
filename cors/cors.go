8a78a70ec82afa2730aae5f37399d898d32437e1
oneilj57 <james.oneill57@mail.dcu.ie>
2021-04-02 15:21:45 +0100

    updated origin for CORS policy

--- cors/cors.go
+++ cors/cors.go
@@ -4,7 +4,7 @@ import "net/http"
 
 /*SetupCORS function will set the http headers required to allow communication with the front end*/
 func SetupCORS(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "https://parkai.herokuapp.com/")
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, ParkAIToken")
 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
