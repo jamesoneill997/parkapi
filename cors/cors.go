b2d7cb441f1dac845fbf046b597068d507454d15
oneilj57 <james.oneill57@mail.dcu.ie>
2021-04-03 13:50:52 +0100

    updated origin cors rules

--- cors/cors.go
+++ cors/cors.go
@@ -4,8 +4,10 @@ import "net/http"
 
 /*SetupCORS function will set the http headers required to allow communication with the front end*/
 func SetupCORS(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	origin := req.Header.Get("Origin")
+
+	(*w).Header().Set("Access-Control-Allow-Origin", origin)
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, ParkAIToken")
 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 }
