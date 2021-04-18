92204f0f7d5df774ae14fc286a67f1179dea57bf
oneilj57 <james.oneill57@mail.dcu.ie>
2021-04-18 18:23:27 +0100

    added debug

--- middleware/authorisation.go
+++ middleware/authorisation.go
@@ -14,19 +14,20 @@ import (
 
 /*GetAuth will check the current actor's cookie jar to see if there is a parkai token present*/
 func GetAuth(r *http.Request) (structs.Claims, error) {
+	var tknStr string
+	var claims = structs.Claims{}
 	// check current session cookies
 	_, err := r.Cookie("ParkAIToken")
-	fmt.Println(r.Header.Get("Set-Cookie"))
 
 	if err != nil {
-		// Unauthorised or bad request
-		fmt.Println(err)
-		return structs.Claims{}, err
+		// Get JWT string from cookie
+		tknStrArr := strings.Split(r.Header.Get("Set-Cookie"), "=")
+		if len(tknStrArr) > 0 {
+			tknStr = tknStrArr[1]
+		} else {
+			return claims, err
+		}
 	}
-
-	// Get JWT string from cookie
-	tknStr := strings.Split(r.Header.Get("Set-Cookie"), "=")[1]
-	claims := &structs.Claims{}
 
 	//parse error return secret env variable
 	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
