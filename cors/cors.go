8b3cf30dca74fbf563630e741c7588a55eddf9d6
oneilj57 <james.oneill57@mail.dcu.ie>
2021-04-20 10:53:42 +0100

    switched logic to local dev

--- cors/cors.go
+++ cors/cors.go
@@ -1,14 +1,12 @@
 package cors
 
 import (
-	"fmt"
 	"net/http"
 )
 
 /*SetupCORS function will set the http headers required to allow communication with the front end*/
 func SetupCORS(w *http.ResponseWriter, req *http.Request) {
 	origin := req.Header.Get("Origin")
-	fmt.Println(origin)
 
 	(*w).Header().Set("Access-Control-Allow-Origin", origin)
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
--- middleware/authorisation.go
+++ middleware/authorisation.go
@@ -2,10 +2,8 @@ package middleware
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
 	"os"
-	"strings"
 
 	"github.com/jamesoneill997/parkapi/structs"
 
@@ -15,31 +13,27 @@ import (
 /*GetAuth will check the current actor's cookie jar to see if there is a parkai token present*/
 func GetAuth(r *http.Request) (structs.Claims, error) {
 	// check current session cookies
-	_, err := r.Cookie("ParkAIToken")
-	fmt.Println(r.Header.Get("Set-Cookie"))
-
-	// Get JWT string from cookie
-	tknStr := strings.Split(r.Header.Get("Set-Cookie"), "=")[1]
-	claims := &structs.Claims{}
+	c, err := r.Cookie("ParkAIToken")
 
 	if err != nil {
 		// Unauthorised or bad request
 		return structs.Claims{}, err
 	}
 
+	// Get JWT string from cookie
+	tknStr := c.Value
+	claims := &structs.Claims{}
+
 	//parse error return secret env variable
 	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
 		return []byte(os.Getenv("secret")), nil
 	})
-
 	if err != nil {
 		return structs.Claims{}, err
-
 	}
 	//unauthorised
 	if !tkn.Valid {
 		return structs.Claims{}, errors.New("InvalidToken")
 	}
-
 	return *claims, nil
 }
